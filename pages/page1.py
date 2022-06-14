13d2535b0a59bc2f51a14f362c45e7c847b66ca5
ismet aksoy <ismetjr@gmail.com>
2022-06-14 15:39:20 +0200

    blanco pages

--- pages/page1.py
+++ pages/page1.py
@@ -1,10 +1,1 @@
 import streamlit as st
-#from streamlit_app import name, account_number
-
-
-account_number = st.text_input('Account Number', 
-                                key = 'account_number', 
-                                value = st.session_state['account_number'] if 'account_number' in st.session_state else '')
-
-
-(st.session_state)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,2 @@
 import streamlit as st
 
-
-
-text = st.write('Homepage')
-
-
-
-
-name = st.text_input('What is your name?', key = 'name', value = st.session_state['name'] if 'name' in st.session_state else '')
-
-st.write(st.session_state)
